a555d40c3a99a1f4300110e6933d58be991c3321
deveshpatwa <119609993+deveshpatwa@users.noreply.github.com>
2024-04-13 20:47:00 +0530

    update

--- bank loan/devesh.py
+++ bank loan/devesh.py
@@ -9,15 +9,4 @@ def fix_col(df):
     df.rename(d, axis=1, inplace=True)
     return df
 
-def fix_space(df):
-    l = list(df.columns)
-    l1 = []
-    for i in l:
-        a= i.lower().replace('_',' ')
-        a = a.replace('-',' ')
-        l1.append(a)
-    d = dict(zip(l,l1))
-    df.rename(d, axis=1, inplace=True)
-    return df
 
-print('sucessfull')
